51c924fb00df060c4577d34013855d52981fe0b5
Nick Goh <166708778+newmaldenite@users.noreply.github.com>
2025-01-17 10:02:49 +0000

    removes function code from main.js, adds challenge instructions

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 import { zone1Stations } from "./data.js";
 
+// ╔╗
+// ║║
+// ║║
+// ╚╝
+// ╔╗
+// ╚╝
+// Greetings Adventurer! Your mission, should you choose to accept it, is simple:
+// 
+// Find which London Underground stations are entirely underground!
+// 
+// While this sounds mad, 60% of the London Underground is actually above ground;
+// only a few stations have no buildings above ground.
+//
+// You've been provided with a dataset in the form of the { zone1Stations } object.
+// This has information about (nearly) all the stations in Zone 1.
+// 
+// Your task is to query the object and return an array containing all the stations
+// with a 'null' RoofRepair property - why repair the roof if you don't have one!
+// 
+// Good luck!
+
 export function isStationUnderground() {
-        const stationsWithNullRepair = [];
-        for (const station in zone1Stations) {
-          // Attempt to check if RoofRepair is null directly
-          if (zone1Stations[station]?.Information?.RoofRepair === null) {
-            stationsWithNullRepair.push(station);
-          }
-        }
-        return stationsWithNullRepair;
+
 }
 
